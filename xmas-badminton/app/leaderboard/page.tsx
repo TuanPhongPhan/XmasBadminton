3783364ad54ef35ede4d999335a4b44c58314d95
TuanPhongPhan <tuanphong.de@gmail.com>
2025-12-24 13:48:40 +0100

    feat: initial leaderboard and matches pages

--- xmas-badminton/app/leaderboard/page.tsx
+++ xmas-badminton/app/leaderboard/page.tsx
@@ -4,7 +4,7 @@ import LeaderboardClient from "./LeaderboardClient";
 export default function Page() {
     return (
         <Suspense fallback={<div style={{ padding: 24 }}>Loading leaderboard…</div>}>
-    <LeaderboardClient />
-    </Suspense>
-);
+            <LeaderboardClient />
+        </Suspense>
+    );
 }
